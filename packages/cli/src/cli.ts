49cf8b00ac2f3bfe29d83052bfede17b5cd39f1e
Lucas <lucasnrgaard@gmail.com>
2025-02-23 20:01:26 +0100

    feat(adapters): add new adapter versions and resolveAdapter function

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -1,8 +1,14 @@
 import type { SHORTCODE_PROVIDERS_SCHEMA } from "@mojis/internal-utils/schemas";
 import type { z } from "zod";
 import process from "node:process";
-import { getAllEmojiVersions, getLatestEmojiVersion, mapEmojiVersionToUnicodeVersion } from "@mojis/internal-utils";
-import { red, yellow } from "farver/fast";
+import { resolveAdapter } from "@mojis/adapters";
+import {
+  getAllEmojiVersions,
+  getLatestEmojiVersion,
+  mapEmojiVersionToUnicodeVersion,
+  MojisNotImplemented,
+} from "@mojis/internal-utils";
+import { green, red, yellow } from "farver/fast";
 import semver from "semver";
 import yargs, { type Argv } from "yargs";
 import pkg from "../package.json" with { type: "json" };
@@ -140,189 +146,184 @@ cli.command(
     console.info("generating emoji data for versions", versions.map((v) => yellow(v.emoji_version)).join(", "));
     console.info(`using the following generators ${args.generators.map((g) => yellow(g)).join(", ")}`);
 
-    // const promises = versions.map(async (version) => {
-    //   const coerced = semver.coerce(version);
+    const promises = versions.map(async (_version) => {
+      console.log(_version);
+      const adapter = resolveAdapter(coerced.version);
 
-    //   if (coerced == null) {
-    //     throw new Error(`invalid version ${version}`);
-    //   }
+      //   if (adapter == null) {
+      //     throw new Error(`no adapter found for version ${version}`);
+      //   }
 
-    //   const adapter = resolveAdapter(coerced.version);
+      //   const lockfileMetadata = lockfile.versions.find((v) => v.emoji_version === version)?.metadata ?? {
+      //     emojis: null,
+      //     metadata: null,
+      //     sequences: null,
+      //     shortcodes: null,
+      //     unicodeNames: null,
+      //     variations: null,
+      //     zwj: null,
+      //   };
 
-    //   if (adapter == null) {
-    //     throw new Error(`no adapter found for version ${version}`);
-    //   }
+      //   if (isGeneratorEnabled("metadata")) {
+      //     if (adapter.metadata == null) {
+      //       throw new MojisNotImplemented("metadata");
+      //     }
 
-    //   const lockfileMetadata = lockfile.versions.find((v) => v.emoji_version === version)?.metadata ?? {
-    //     emojis: null,
-    //     metadata: null,
-    //     sequences: null,
-    //     shortcodes: null,
-    //     unicodeNames: null,
-    //     variations: null,
-    //     zwj: null,
-    //   };
+      //     const { groups, emojiMetadata } = await adapter.metadata({
+      //       emojiVersion: version,
+      //       force,
+      //       unicodeVersion: getUnicodeVersionByEmojiVersion(version)!,
+      //       lockfileMetadata,
+      //     });
 
-    //   if (isGeneratorEnabled("metadata")) {
-    //     if (adapter.metadata == null) {
-    //       throw new MojisNotImplemented("metadata");
-    //     }
+      //     await fs.ensureDir(`./data/v${version}/metadata`);
 
-    //     const { groups, emojiMetadata } = await adapter.metadata({
-    //       emojiVersion: version,
-    //       force,
-    //       unicodeVersion: getUnicodeVersionByEmojiVersion(version)!,
-    //       lockfileMetadata,
-    //     });
+      //     await fs.writeFile(
+      //       `./data/v${version}/groups.json`,
+      //       JSON.stringify(groups, null, 2),
+      //       "utf-8",
+      //     );
 
-    //     await fs.ensureDir(`./data/v${version}/metadata`);
+      //     await Promise.all(Object.entries(emojiMetadata).map(([group, metadata]) => fs.writeFile(
+      //       `./data/v${version}/metadata/${group}.json`,
+      //       JSON.stringify(metadata, null, 2),
+      //       "utf-8",
+      //     )));
+      //   }
 
-    //     await fs.writeFile(
-    //       `./data/v${version}/groups.json`,
-    //       JSON.stringify(groups, null, 2),
-    //       "utf-8",
-    //     );
+      //   if (isGeneratorEnabled("sequences")) {
+      //     if (adapter.sequences == null) {
+      //       throw new MojisNotImplemented("sequences");
+      //     }
 
-    //     await Promise.all(Object.entries(emojiMetadata).map(([group, metadata]) => fs.writeFile(
-    //       `./data/v${version}/metadata/${group}.json`,
-    //       JSON.stringify(metadata, null, 2),
-    //       "utf-8",
-    //     )));
-    //   }
+      //     const { sequences, zwj } = await adapter.sequences({
+      //       emojiVersion: version,
+      //       force,
+      //       unicodeVersion: getUnicodeVersionByEmojiVersion(version)!,
+      //       lockfileMetadata,
+      //     });
 
-    //   if (isGeneratorEnabled("sequences")) {
-    //     if (adapter.sequences == null) {
-    //       throw new MojisNotImplemented("sequences");
-    //     }
+      //     await fs.ensureDir(`./data/v${version}`);
 
-    //     const { sequences, zwj } = await adapter.sequences({
-    //       emojiVersion: version,
-    //       force,
-    //       unicodeVersion: getUnicodeVersionByEmojiVersion(version)!,
-    //       lockfileMetadata,
-    //     });
+      //     await fs.writeFile(
+      //       `./data/v${version}/zwj-sequences.json`,
+      //       JSON.stringify(zwj, null, 2),
+      //       "utf-8",
+      //     );
 
-    //     await fs.ensureDir(`./data/v${version}`);
+      //     await fs.writeFile(
+      //       `./data/v${version}/sequences.json`,
+      //       JSON.stringify(sequences, null, 2),
+      //       "utf-8",
+      //     );
+      //   }
 
-    //     await fs.writeFile(
-    //       `./data/v${version}/zwj-sequences.json`,
-    //       JSON.stringify(zwj, null, 2),
-    //       "utf-8",
-    //     );
+      //   if (isGeneratorEnabled("variations")) {
+      //     if (adapter.variations == null) {
+      //       throw new MojisNotImplemented("variations");
+      //     }
 
-    //     await fs.writeFile(
-    //       `./data/v${version}/sequences.json`,
-    //       JSON.stringify(sequences, null, 2),
-    //       "utf-8",
-    //     );
-    //   }
+      //     const variations = await adapter.variations({
+      //       emojiVersion: version,
+      //       force,
+      //       unicodeVersion: getUnicodeVersionByEmojiVersion(version)!,
+      //       lockfileMetadata,
+      //     });
 
-    //   if (isGeneratorEnabled("variations")) {
-    //     if (adapter.variations == null) {
-    //       throw new MojisNotImplemented("variations");
-    //     }
+      //     await fs.ensureDir(`./data/v${version}`);
+      //     await fs.writeFile(
+      //       `./data/v${version}/variations.json`,
+      //       JSON.stringify(variations, null, 2),
+      //       "utf-8",
+      //     );
+      //   }
 
-    //     const variations = await adapter.variations({
-    //       emojiVersion: version,
-    //       force,
-    //       unicodeVersion: getUnicodeVersionByEmojiVersion(version)!,
-    //       lockfileMetadata,
-    //     });
+      //   if (isGeneratorEnabled("emojis")) {
+      //     if (adapter.emojis == null) {
+      //       throw new MojisNotImplemented("emojis");
+      //     }
 
-    //     await fs.ensureDir(`./data/v${version}`);
-    //     await fs.writeFile(
-    //       `./data/v${version}/variations.json`,
-    //       JSON.stringify(variations, null, 2),
-    //       "utf-8",
-    //     );
-    //   }
+      //     const { emojiData, emojis } = await adapter.emojis({
+      //       emojiVersion: version,
+      //       force,
+      //       unicodeVersion: getUnicodeVersionByEmojiVersion(version)!,
+      //       lockfileMetadata,
+      //     });
 
-    //   if (isGeneratorEnabled("emojis")) {
-    //     if (adapter.emojis == null) {
-    //       throw new MojisNotImplemented("emojis");
-    //     }
+      //     await fs.ensureDir(`./data/v${version}`);
 
-    //     const { emojiData, emojis } = await adapter.emojis({
-    //       emojiVersion: version,
-    //       force,
-    //       unicodeVersion: getUnicodeVersionByEmojiVersion(version)!,
-    //       lockfileMetadata,
-    //     });
+      //     await fs.writeFile(
+      //       `./data/v${version}/emoji-data.json`,
+      //       JSON.stringify(emojiData, null, 2),
+      //       "utf-8",
+      //     );
 
-    //     await fs.ensureDir(`./data/v${version}`);
+      //     for (const [group, subgroup] of Object.entries(emojis)) {
+      //       await fs.ensureDir(`./data/v${version}/emojis/${group}`);
 
-    //     await fs.writeFile(
-    //       `./data/v${version}/emoji-data.json`,
-    //       JSON.stringify(emojiData, null, 2),
-    //       "utf-8",
-    //     );
+      //       for (const hexcodes of Object.values(subgroup)) {
+      //         await fs.ensureDir(`./data/v${version}/emojis/${group}/${subgroup}`);
 
-    //     for (const [group, subgroup] of Object.entries(emojis)) {
-    //       await fs.ensureDir(`./data/v${version}/emojis/${group}`);
+      //         for (const [hexcode, emoji] of Object.entries(hexcodes)) {
+      //           await fs.writeFile(
+      //             `./data/v${version}/emojis/${group}/${subgroup}/${hexcode}.json`,
+      //             JSON.stringify(emoji, null, 2),
+      //             "utf-8",
+      //           );
+      //         }
+      //       }
+      //     }
+      //   }
 
-    //       for (const hexcodes of Object.values(subgroup)) {
-    //         await fs.ensureDir(`./data/v${version}/emojis/${group}/${subgroup}`);
+      //   if (isGeneratorEnabled("shortcodes")) {
+      //     const providers = await parseAsync(SHORTCODE_PROVIDERS_SCHEMA, args["shortcode-providers"]);
 
-    //         for (const [hexcode, emoji] of Object.entries(hexcodes)) {
-    //           await fs.writeFile(
-    //             `./data/v${version}/emojis/${group}/${subgroup}/${hexcode}.json`,
-    //             JSON.stringify(emoji, null, 2),
-    //             "utf-8",
-    //           );
-    //         }
-    //       }
-    //     }
-    //   }
+      //     if (providers.length === 0) {
+      //       throw new Error("no shortcode providers specified");
+      //     }
 
-    //   if (isGeneratorEnabled("shortcodes")) {
-    //     const providers = await parseAsync(SHORTCODE_PROVIDERS_SCHEMA, args["shortcode-providers"]);
+      //     if (adapter.shortcodes == null) {
+      //       throw new MojisNotImplemented("shortcodes");
+      //     }
 
-    //     if (providers.length === 0) {
-    //       throw new Error("no shortcode providers specified");
-    //     }
+      //     const shortcodes = await adapter.shortcodes({
+      //       emojiVersion: version,
+      //       force,
+      //       unicodeVersion: getUnicodeVersionByEmojiVersion(version)!,
+      //       providers,
+      //       lockfileMetadata,
+      //     });
 
-    //     if (adapter.shortcodes == null) {
-    //       throw new MojisNotImplemented("shortcodes");
-    //     }
+      //     await fs.ensureDir(`./data/v${version}/shortcodes`);
 
-    //     const shortcodes = await adapter.shortcodes({
-    //       emojiVersion: version,
-    //       force,
-    //       unicodeVersion: getUnicodeVersionByEmojiVersion(version)!,
-    //       providers,
-    //       lockfileMetadata,
-    //     });
+      //     for (const provider of providers) {
+      //       if (shortcodes[provider] == null) {
+      //         consola.warn(`no shortcodes found for provider ${provider}`);
+      //         continue;
+      //       }
 
-    //     await fs.ensureDir(`./data/v${version}/shortcodes`);
+      //       await fs.writeFile(
+      //         `./data/v${version}/shortcodes/${provider}.json`,
+      //         JSON.stringify(shortcodes[provider], null, 2),
+      //         "utf-8",
+      //       );
+      //     }
+      //   }
+    });
 
-    //     for (const provider of providers) {
-    //       if (shortcodes[provider] == null) {
-    //         consola.warn(`no shortcodes found for provider ${provider}`);
-    //         continue;
-    //       }
+    const results = await Promise.allSettled(promises);
 
-    //       await fs.writeFile(
-    //         `./data/v${version}/shortcodes/${provider}.json`,
-    //         JSON.stringify(shortcodes[provider], null, 2),
-    //         "utf-8",
-    //       );
-    //     }
-    //   }
-    // });
+    for (const result of results) {
+      if (result.status === "rejected") {
+        if (result.reason instanceof MojisNotImplemented) {
+          console.warn(result.reason.message);
+          continue;
+        }
+        console.error(result.reason);
+      }
+    }
 
-    // const results = await Promise.allSettled(promises);
-
-    // for (const result of results) {
-    //   if (result.status === "rejected") {
-    //     if (result.reason instanceof MojisNotImplemented) {
-    //       consola.warn(result.reason.message);
-    //       continue;
-    //     }
-    //     consola.error(result.reason);
-    //   }
-    // }
-
-    // consola.info(green("done"));
+    console.info(green("done"));
   },
 );
 
--- packages/internal-utils/src/index.ts
+++ packages/internal-utils/src/index.ts
@@ -1,3 +1,10 @@
 export * from "./cache";
 export * from "./hexcode";
 export * from "./versions";
+
+export class MojisNotImplemented extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "MojisNotImplemented";
+  }
+}
