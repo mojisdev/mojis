17be0c64ebef80524bd1eaa3b365c4adba455283
Lucas <lucasnrgaard@gmail.com>
2025-03-02 08:21:14 +0100

    feat(adapter): implement sequence URLs and transform function

--- packages/adapters/src/adapters/modern/adapter.ts
+++ packages/adapters/src/adapters/modern/adapter.ts
@@ -1,85 +1,79 @@
-import {
-  type EmojiSequence,
-  expandHexRange,
-  fetchCache,
-} from "@mojis/internal-utils";
-import {
-  FEMALE_SIGN,
-  MALE_SIGN,
-} from "@mojis/internal-utils/constants";
 import { defineMojiAdapter } from "../../define";
-// import { adapterSequenceGroup } from "./sequences";
 
 export const modernAdapter = defineMojiAdapter({
   name: "modern",
   description: "adapter for the modern emoji versions",
   range: ">=11.0.0",
   extend: "base",
-  // sequences: {
-  //   url: (ctx) => [
-  //     `https://unicode.org/Public/emoji/${ctx.emoji_version}/emoji-sequences.txt`,
-  //     `https://unicode.org/Public/emoji/${ctx.emoji_version}/emoji-zwj-sequences`,
-  //   ],
-  //   cacheKey: (ctx) => `v${ctx.emoji_version}/sequences.json`,
-  //   transform: adapterSequenceGroup,
-  // },
-  // async sequences(ctx) {
-  //   const [sequences, zwj] = await Promise.all([
-  //     {
-  //       cacheKey: `v${ctx.versions.emoji_version}/sequences.json`,
-  //       url: `https://unicode.org/Public/emoji/${ctx.versions.emoji_version}/emoji-sequences.txt`,
-  //     },
-  //     {
-  //       cacheKey: `v${ctx.versions.emoji_version}/zwj-sequences.json`,
-  //       url: `https://unicode.org/Public/emoji/${ctx.versions.emoji_version}/emoji-zwj-sequences.txt`,
-  //     },
-  //   ].map(async ({ cacheKey, url }) => {
-  //     return await fetchCache(url, {
-  //       cacheKey,
-  //       parser(data) {
-  //         const lines = data.split("\n");
+  sequences: {
+    urls(ctx) {
+      return [
+        {
+          url: `https://unicode.org/Public/emoji/${ctx.emoji_version}/emoji-sequences.txt`,
+          cacheKey: `v${ctx.emoji_version}/sequences.json`,
+        },
+        {
+          url: `https://unicode.org/Public/emoji/${ctx.emoji_version}/emoji-zwj-sequences`,
+          cacheKey: `v${ctx.emoji_version}/zwj-sequences.json`,
+        },
+      ];
+    },
+    transform(ctx, data) {
+      if (data == null) {
+        return {
+          sequences: [],
+          zwj: [],
+        };
+      }
 
-  //         const sequences: EmojiSequence[] = [];
+      // const sequences: EmojiSequence[] = [];
 
-  //         for (let line of lines) {
-  //           // skip empty line & comments
-  //           if (line.trim() === "" || line.startsWith("#")) {
-  //             continue;
-  //           }
+      // for (const _data of data) {
+      //   if (_data == null) {
+      //     throw new Error("invalid data");
+      //   }
 
-  //           // remove line comment
-  //           const commentIndex = line.indexOf("#");
-  //           if (commentIndex !== -1) {
-  //             line = line.slice(0, commentIndex).trim();
-  //           }
+      //   const lines = data.split("\n");
 
-  //           const [hex, property, description] = line.split(";").map((col) => col.trim()).slice(0, 4);
+      //   const sequences: EmojiSequence[] = [];
 
-  //           if (hex == null || property == null || description == null) {
-  //             throw new Error(`invalid line: ${line}`);
-  //           }
+      //   for (let line of lines) {
+      //     // skip empty line & comments
+      //     if (line.trim() === "" || line.startsWith("#")) {
+      //       continue;
+      //     }
 
-  //           const expandedHex = expandHexRange(hex);
+      //     // remove line comment
+      //     const commentIndex = line.indexOf("#");
+      //     if (commentIndex !== -1) {
+      //       line = line.slice(0, commentIndex).trim();
+      //     }
 
-  //           for (const hex of expandedHex) {
-  //             sequences.push({
-  //               hex: hex.replace(/\s+/g, "-"),
-  //               property,
-  //               description,
-  //               gender: hex.includes(FEMALE_SIGN) ? "female" : hex.includes(MALE_SIGN) ? "male" : null,
-  //             });
-  //           }
-  //         }
+      //     const [hex, property, description] = line.split(";").map((col) => col.trim()).slice(0, 4);
 
-  //         return sequences;
-  //       },
-  //       bypassCache: ctx.force,
-  //     });
-  //   }));
+      //     if (hex == null || property == null || description == null) {
+      //       throw new Error(`invalid line: ${line}`);
+      //     }
 
-  //   return {
-  //     sequences: sequences || [],
-  //     zwj: zwj || [],
-  //   };
-  // },
+      //     const expandedHex = expandHexRange(hex);
+
+      //     for (const hex of expandedHex) {
+      //       sequences.push({
+      //         hex: hex.replace(/\s+/g, "-"),
+      //         property,
+      //         description,
+      //         gender: hex.includes(FEMALE_SIGN) ? "female" : hex.includes(MALE_SIGN) ? "male" : null,
+      //       });
+      //     }
+      //   }
+
+      //   return sequences;
+      // }
+
+      return {
+        sequences: [],
+        zwj: [],
+      };
+    },
+  },
 });
