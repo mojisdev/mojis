47f947f81dc6ba244e64a27902f4506ea06664f7
Lucas <lucasnrgaard@gmail.com>
2025-03-02 16:12:47 +0100

    test: organise tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,19 @@
+import { existsSync, readdirSync } from 'node:fs';
 import { defineConfig } from 'vitest/config'
+
+const pkgRoot = (pkg: string) =>
+  new URL(`./packages/${pkg}`, import.meta.url).pathname;
+const alias = (pkg: string) => `${pkgRoot(pkg)}/src`;
+
+const aliases = readdirSync(new URL("./packages", import.meta.url).pathname)
+  .filter((dir) => existsSync(pkgRoot(dir) + "/package.json"))
+  .reduce<Record<string, string>>(
+    (acc, pkg) => {
+      acc[`@mojis/${pkg}`] = alias(pkg);
+      return acc;
+    },
+    {});
+
 
 export default defineConfig({
   test: {
@@ -6,5 +21,43 @@ export default defineConfig({
       provider: "v8",
       include: ["**/src/**"],
     },
+    setupFiles: [
+      "./test/setup/fetch-mock.ts"
+    ],
+    workspace: [
+      {
+        extends: true,
+        test: {
+          include: ["./packages/internal-utils/**/*.test.{ts,js}"],
+          name: "internal-utils",
+          environment: "node",
+          mockReset: true,
+        },
+        esbuild: { target: "es2020" },
+        resolve: { alias: aliases },
+      },
+      {
+        extends: true,
+        test: {
+          include: ["./packages/adapters/**/*.test.{ts,js}"],
+          name: "adapters",
+          environment: "node",
+          mockReset: true,
+        },
+        esbuild: { target: "es2020" },
+        resolve: { alias: aliases },
+      },
+      {
+        extends: true,
+        test: {
+          include: ["./packages/cli/**/*.test.{ts,js}"],
+          name: "cli",
+          environment: "node",
+          mockReset: true,
+        },
+        esbuild: { target: "es2020" },
+        resolve: { alias: aliases },
+      }
+    ]
   }
-});
+})
