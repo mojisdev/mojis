6c015aaa1125471fa1d75643cb47fb230ca829b2
Lucas <lucasnrgaard@gmail.com>
2025-03-02 09:23:12 +0100

    revert

--- packages/adapters/src/adapters/modern/sequences.ts
+++
@@ -1,7 +0,0 @@
-// import type { AdapterSequenceGroup } from "../../types";
-// import { defineAdapterGroupFn } from "../../define";
-
-// export const adapterSequenceGroup = defineAdapterGroupFn<AdapterSequenceGroup, "transform">((ctx, data) => {
-//   console.log(ctx, data);
-//   return [[], []];
-// });
