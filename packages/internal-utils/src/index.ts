1030987d0ea726690052ad8608930e61e24f8d65
Lucas <lucasnrgaard@gmail.com>
2025-03-07 09:14:03 +0100

    feat: export predicates

--- packages/internal-utils/src/index.ts
+++ packages/internal-utils/src/index.ts
@@ -3,6 +3,7 @@
 export * from "./cache";
 export * from "./constants";
 export * from "./hexcode";
+export * from "./predicates";
 export * from "./shortcodes";
 export type * from "./types";
 export * from "./versions";
