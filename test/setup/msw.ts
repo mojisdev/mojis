67c38d5a36948d0809330cff3448291eeabe2298
Lucas Nørgård <lucasnrgaard@gmail.com>
2025-03-11 05:17:48 +0100

    chore: fix tests

--- test/setup/msw.ts
+++ test/setup/msw.ts
@@ -1,6 +1,6 @@
 import { afterAll, afterEach, beforeAll } from "vitest";
-import { msw_server } from "../msw-utils/msw";
+import { MSW_SERVER } from "../msw-utils/msw";
 
-beforeAll(() => msw_server.listen({ onUnhandledRequest: "warn" }));
-afterAll(() => msw_server.close());
-afterEach(() => msw_server.resetHandlers());
+beforeAll(() => MSW_SERVER.listen({ onUnhandledRequest: "warn" }));
+afterAll(() => MSW_SERVER.close());
+afterEach(() => MSW_SERVER.resetHandlers());
