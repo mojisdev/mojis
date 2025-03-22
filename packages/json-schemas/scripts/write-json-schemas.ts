102d519f1fa7b406cea34156e3e5ca48efb2bea3
Lucas Nørgård <lucasnrgaard@gmail.com>
2025-03-22 17:33:47 +0100

    chore: improve schema script

--- packages/json-schemas/scripts/write-json-schemas.ts
+++ packages/json-schemas/scripts/write-json-schemas.ts
@@ -1,4 +1,4 @@
-import { mkdir, writeFile } from "node:fs/promises";
+import { mkdir, rm, writeFile } from "node:fs/promises";
 import process from "node:process";
 import { EMOJI_GROUPS_SCHEMA } from "@mojis/internal-utils/schemas";
 import { zodToJsonSchema } from "zod-to-json-schema";
@@ -6,10 +6,15 @@ import { zodToJsonSchema } from "zod-to-json-schema";
 const root = new URL("../", import.meta.url);
 
 async function run() {
+  console.log("cleaning up schemas...");
+  await rm(`${root.pathname}/schemas`, { recursive: true, force: true });
+
+  console.log("writing schemas...");
   const groups = zodToJsonSchema(EMOJI_GROUPS_SCHEMA);
 
   await mkdir(`${root.pathname}/schemas`, { recursive: true });
   await writeFile(`${root.pathname}/schemas/groups.json`, JSON.stringify(groups, null, 2));
+  console.log("done!");
 }
 
 run().catch((err) => {
