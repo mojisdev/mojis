bdf0381025d2904cb724abf412387fc27a52be1d
Lucas Nørgård <lucasnrgaard@gmail.com>
2025-03-23 08:04:51 +0100

    feat: enhance emoji metadata parsing with group and subgroup support

--- packages/adapters/src/handlers/metadata.ts
+++ packages/adapters/src/handlers/metadata.ts
@@ -35,9 +35,98 @@ export const handler = builder
         return data.split("\n");
       })
       .transform((ctx, lines) => {
+        let currentGroup: EmojiGroup | undefined;
+
+        const groups: EmojiGroup[] = [];
+
+        // [group-subgroup][hexcode] = metadata
+        const emojis: Record<string, Record<string, EmojiMetadata>> = {};
+
+        for (const line of lines) {
+          if (line.trim() === "") {
+            continue;
+          }
+
+          if (line.startsWith("# group:")) {
+            const groupName = line.slice(8).trim();
+
+            const group: EmojiGroup = {
+              name: groupName,
+              slug: slugify(groupName),
+              subgroups: [],
+            };
+
+            currentGroup = group;
+
+            groups.push(group);
+
+            continue;
+          } else if (line.startsWith("# subgroup:")) {
+            const subgroupName = line.slice(11).trim();
+
+            if (currentGroup == null) {
+              throw new Error(`subgroup ${subgroupName} without group`);
+            }
+
+            currentGroup.subgroups.push(slugify(subgroupName));
+
+            continue;
+          } else if (line.startsWith("#")) {
+            continue;
+          }
+
+          const [baseHexcode, trailingLine] = line.split(";");
+
+          if (baseHexcode == null || trailingLine == null) {
+            throw new Error(`invalid line: ${line}`);
+          }
+
+          const [baseQualifier, comment] = trailingLine.split("#");
+
+          if (baseQualifier == null || comment == null) {
+            throw new Error(`invalid line: ${line}`);
+          }
+
+          const hexcode = baseHexcode.trim().replace(/\s+/g, "-");
+          const qualifier = baseQualifier.trim();
+
+          // if the emoji_version is v5 and under.
+          // the content after the # doesn't include the emoji version.
+          let emoji;
+          let trimmedComment;
+
+          const extractedEmojiVersion = extractEmojiVersion(comment.trim());
+
+          if (isBefore(ctx.emoji_version, "6.0.0")) {
+            [emoji, trimmedComment] = comment.trim().split(" ");
+          } else {
+            [emoji, trimmedComment] = comment.trim().split(` E${extractedEmojiVersion} `);
+          }
+
+          const groupName = currentGroup?.slug ?? "unknown";
+          const subgroupName = currentGroup?.subgroups[currentGroup.subgroups.length - 1] ?? "unknown";
+
+          const metadataGroup = `${groupName}-${subgroupName}`;
+
+          if (emojis[metadataGroup] == null) {
+            emojis[metadataGroup] = {};
+          }
+
+          emojis[metadataGroup][hexcode] = {
+            group: groupName,
+            subgroup: subgroupName,
+            qualifier,
+            emojiVersion: extractedEmojiVersion || null,
+            unicodeVersion: extractUnicodeVersion(extractedEmojiVersion, ctx.unicode_version),
+            description: trimmedComment || "",
+            emoji: emoji || null,
+            hexcodes: hexcode.split("-"),
+          };
+        }
+
         return {
-          groups: [],
-          emojis: {},
+          groups,
+          emojis,
         };
       })
       .output((ctx, transformed) => {
