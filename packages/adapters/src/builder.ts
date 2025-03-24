b707af432b119257bfe327706cc4ef9460584847
Lucas Nørgård <lucasnrgaard@gmail.com>
2025-03-24 06:04:14 +0100

    dump

--- packages/adapters/src/builder.ts
+++ packages/adapters/src/builder.ts
@@ -1,10 +1,23 @@
+import type { WriteCacheOptions } from "@mojis/internal-utils";
 import type {
+  AdapterContext,
   AdapterHandlerBuilder,
   AdapterHandlerType,
+  AdapterUrls,
+  AggregateFn,
   AnyAdapterHandler,
   AnyHandleVersionParams,
+  BuiltinParser,
+  GetParseOptionsFromParser,
+  GetParseOutputFromBuiltInParser,
+  HandleVersionBuilder,
   NormalizedVersionHandler,
+  OutputFn,
+  ParserFn,
   PredicateFn,
+  TransformFn,
+  UnsetMarker,
+  WrapInContextFn,
 } from "./types";
 
 function internalCreateAdapterHandlerBuilder<TAdapterType extends AdapterHandlerType>(
@@ -36,6 +49,67 @@ function internalCreateAdapterHandlerBuilder<TAdapterType extends AdapterHandler
   };
 }
 
+function internalCreateVersionHandlerBuilder<TParams extends AnyHandleVersionParams>(
+  initDef: Partial<NormalizedVersionHandler<TParams>> = {},
+): HandleVersionBuilder<TParams> {
+  const _def: NormalizedVersionHandler<TParams> = {
+    urls: () => undefined,
+    parser: "generic",
+    parserOptions: undefined,
+    transform: (_, data) => data,
+    aggregate: (_, data) => data,
+    output: (_, data) => data,
+    cacheOptions: {},
+    fetchOptions: {},
+    ...initDef,
+  };
+
+  return {
+    urls(userUrls) {
+      return internalCreateVersionHandlerBuilder({
+        ..._def,
+        urls: userUrls,
+      });
+    },
+    parser(userParser) {
+      return internalCreateVersionHandlerBuilder({
+        ..._def,
+        parser: userParser,
+      });
+    },
+    transform(userTransform) {
+      return internalCreateVersionHandlerBuilder({
+        ..._def,
+        transform: userTransform,
+      });
+    },
+    aggregate(userAggregate) {
+      return internalCreateVersionHandlerBuilder({
+        ..._def,
+        aggregate: userAggregate,
+      });
+    },
+    output(userOutput) {
+      return internalCreateVersionHandlerBuilder({
+        ..._def,
+        output: userOutput,
+      });
+    },
+    cacheOptions(userCacheOptions) {
+      return internalCreateVersionHandlerBuilder({
+        ..._def,
+        cacheOptions: userCacheOptions,
+      });
+    },
+    fetchOptions(userFetchOptions) {
+      return internalCreateVersionHandlerBuilder({
+        ..._def,
+        fetchOptions: userFetchOptions,
+      });
+    },
+  };
+}
+
 export interface CreateBuilderOptions<TAdapterType extends AdapterHandlerType> {
   type: TAdapterType;
 }
@@ -49,4 +123,10 @@ export function createAdapterHandlerBuilder<TAdapterType extends AdapterHandlerT
   return internalCreateAdapterHandlerBuilder<TAdapterType>({
     adapterType: opts?.type,
   });
-};
+}
+
+export function createVersionHandlerBuilder<TParams extends AnyHandleVersionParams>(
+  initDef: Partial<NormalizedVersionHandler<TParams>> = {},
+): HandleVersionBuilder<TParams> {
+  return internalCreateVersionHandlerBuilder<TParams>(initDef);
+}
