703b217fbcbb3692862ca2c4d1eb61a89d3332ec
Lucas Nørgård <lucasnrgaard@gmail.com>
2025-04-02 05:51:10 +0100

    chore: lint

--- packages/adapters/test/adapter-handler-builder.test.ts
+++ packages/adapters/test/adapter-handler-builder.test.ts
@@ -1,5 +1,5 @@
 import type { AdapterContext } from "../src/global-types";
-import { describe, expect, it, vi } from "vitest";
+import { describe, expect, it } from "vitest";
 import { z } from "zod";
 import { createAdapterHandlerBuilder } from "../src/adapter-builder/builder";
 
