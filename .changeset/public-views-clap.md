9e975238670f5a68a6ace93932e50c158d3b90d0
Lucas <lucasnrgaard@gmail.com>
2025-04-18 15:58:15 +0200

    feat: add automatic generation of json schema

---
+++ .changeset/public-views-clap.md
@@ -0,0 +1,5 @@
+---
+"@mojis/json-schemas": minor
+---
+
+add json schemas for more sources
--- packages/json-schemas/scripts/write-json-schemas.ts
+++ packages/json-schemas/scripts/write-json-schemas.ts
@@ -1,6 +1,6 @@
-import { mkdir, rm, writeFile } from "node:fs/promises";
+import { mkdir, readFile, rm, writeFile } from "node:fs/promises";
 import process from "node:process";
-import { EMOJI_GROUPS_SCHEMA, GROUPED_BY_HEXCODE_EMOJI_METADATA_SCHEMA } from "@mojis/schemas/emojis";
+import * as sourceAdapters from "../../adapters/src/handlers/source";
 
 const root = new URL("../", import.meta.url);
 
@@ -9,13 +9,41 @@ async function run() {
   await rm(`${root.pathname}/schemas`, { recursive: true, force: true });
 
   console.log("writing schemas...");
-  const groups = EMOJI_GROUPS_SCHEMA.toJsonSchema();
-  const transformedEmojis = GROUPED_BY_HEXCODE_EMOJI_METADATA_SCHEMA.toJsonSchema();
 
-  await mkdir(`${root.pathname}/schemas`, { recursive: true });
-  await writeFile(`${root.pathname}/schemas/groups.json`, JSON.stringify(groups, null, 2));
-  await writeFile(`${root.pathname}/schemas/emojis.json`, JSON.stringify(transformedEmojis, null, 2));
-  console.log("done!");
+  const pkg = JSON.parse(await readFile(`${root.pathname}/package.json`, "utf-8"));
+
+  const defaultExports = {
+    "./package.json": "./package.json",
+  };
+  const exports: Record<string, string> = {};
+
+  for (const [name, handler] of Object.entries(sourceAdapters)) {
+    if (handler == null) {
+      console.log(`skipping ${name}...`);
+      continue;
+    }
+
+    await mkdir(`${root.pathname}/schemas`, { recursive: true });
+    for (const [key, value] of Object.entries(handler.persistence.schemas)) {
+      await writeFile(
+        `${root.pathname}/schemas/${handler.adapterType}-${key}.json`,
+        JSON.stringify(value.schema.toJsonSchema(), null, 2),
+      );
+      exports[`./${handler.adapterType}-${key}.json`] = `./schemas/${handler.adapterType}-${key}.json`;
+      console.log(`wrote ${handler.adapterType}-${key}.json`);
+    }
+  }
+
+  await writeFile(
+    `${root.pathname}/package.json`,
+    `${JSON.stringify({
+      ...pkg,
+      exports: {
+        ...exports,
+        ...defaultExports,
+      },
+    }, null, 2)}\n`,
+  );
 }
 
 run().catch((err) => {
